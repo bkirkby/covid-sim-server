0b7ee74013685577aa002ade6f1b0002e5a5ff41
brian kirkby <bkirkby@gitlab.shoc.us>
2020-10-14 07:05:58 -0600

    added endpoints

--- index.js
+++ index.js
@@ -1,31 +1,71 @@
 const express = require('express')
 const morgan = require('morgan')
 const logger = require('./logger')
+var cors = require('cors')
+const graphService = require('./graphService')
 require('dotenv').config()
+
+const corsWhiteList = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'https://covidsim.now.sh', 
+  undefined
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    console.log('bk: index.js: corsOptions: origin: ', origin)
+    if (corsWhiteList.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('not allowed by cors'));
+    }
+  }
+}
 
 const app = express()
 
 const port = 8081
-
+app.use(express.json())
 app.use(morgan('tiny'))
+app.use(cors(corsOptions))
 
 function simpleAuth(req, res, next) {
   logger.debug('simpleAuth called')
   if (req.get("Authorization") === process.env.COVIDSIM_AUTHKEY) {
     next();
   } else {
-    res.status(403).json({message: "admin key not valid"});
+    res.status(403).json({error: "admin key not valid"});
   }
 }
 
-app.get('/login',  simpleAuth,
-  function(req, res) {
-    res.json({status:'ok'})
+app.post('/getGraph', simpleAuth, async (req, res) => {
+  if (!req.body.isolation, !req.body.social_distance, !req.body.population) {
+    res.status(400).json({message: "required params not found: isolation, social_distance, or population"});
   }
-)
+  const graph_data = await graphService.getAvgGraph(req.body);
+  res.status(200).json(graph_data);
+})
 
-app.get('/hello', (req, res) => {
-  res.send('hello, world!')
+app.post('/addGraph', (req, res) => {
+  graphService.addGraphToAvg(req.body)
+  res.send(200);
+})
+
+app.get('/getGraphList', simpleAuth, async (req, res) => {
+  const graph_data = await graphService.getGraphList().catch(err => {
+    res.status(500).json({error: err});
+  });
+  res.status(200).json(graph_data);
+})
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  if (username === process.env.ADMIN_USER && password === process.env.ADMIN_PASSWORD) {
+    res.status(200).json({ key: process.env.COVIDSIM_AUTHKEY });
+  } else {
+    res.status(403).json({ error: 'invalid login' });
+  }
 })
 
 app.listen(port, () => console.log(`app running on port ${port}`))
